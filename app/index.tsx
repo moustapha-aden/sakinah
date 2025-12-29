47d6c76346c17abfb4136b5ad95c1d6d75f9d7d8
moustapha-aden <ton-email-github@exemple.com>
2025-12-29 15:33:22 +0300

    chore: update main entry point and add new dependencies for expo-router and related packages

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Sakinah 🚀</Text>
+
+    </View>
+  );
+}
